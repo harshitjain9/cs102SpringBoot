0baff1c35b9d1b2b57d1078535d67da266ef87d8
charlotte-wt <charlottewee01@gmail.com>
2021-03-23 17:15:39 +0800

    Updated Crud to JpaRepo

--- cs102-g1t2/src/main/java/g1t2/repositories/AccountRepository.java
+++ cs102-g1t2/src/main/java/g1t2/repositories/AccountRepository.java
@@ -1,9 +1,9 @@
 package g1t2.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import g1t2.entities.Account;
 
-public interface AccountRepository extends CrudRepository<Account, String> {
+public interface AccountRepository extends JpaRepository<Account, String> {
 	public Account findByEmail(String email);
 }
