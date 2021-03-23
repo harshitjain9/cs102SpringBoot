2f9d0e61dd74140fba609a098afbfb47f905b0a5
charlotte-wt <charlottewee01@gmail.com>
2021-03-23 17:01:19 +0800

    Created AlertTriggeredController

---
+++ cs102-g1t2/src/main/java/g1t2/controller/AlertTriggeredController.java
@@ -0,0 +1,31 @@
+package g1t2.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import g1t2.entities.AlertTriggered;
+import g1t2.service.AlertTriggeredService;
+
+@RestController
+public class AlertTriggeredController {
+	
+	@Autowired
+	private AlertTriggeredService alertTriggered;
+	
+	//GET
+	@GetMapping("/getAlertsTriggeredAccordingToUser")
+	public List<AlertTriggered> getAlertsTriggeredAccordingToUser(String email) {
+		return alertTriggered.getAlertsTriggeredAccordingToUser(email);
+	}
+	
+	//POST
+	@PostMapping("/createNewAlertTriggered")
+	public AlertTriggered createNewAlertTriggered(AlertTriggered alertTriggered) {
+    	return this.alertTriggered.createNewAlertTriggered(alertTriggered);
+    }
+
+}
