6db53a785ea442ed36786e740210f0ce4b0e08ee
charlotte-wt <charlottewee01@gmail.com>
2021-03-26 21:29:48 +0800

    changedddd
    asd

--- cs102-g1t2/src/main/java/g1t2/repositories/VesselRepository.java
+++ cs102-g1t2/src/main/java/g1t2/repositories/VesselRepository.java
@@ -1,10 +1,18 @@
 package g1t2.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import g1t2.entities.Vessel;
+import g1t2.entities.VesselComposite;
 
-public interface VesselRepository extends JpaRepository<Vessel, String>{
-	public Vessel findByAbbrVoy(String abbrVslM, String VoyN);
-	public Vessel deleteByAbbrVoy(String abbrVslM, String VoyN);
+@Repository
+public interface VesselRepository extends JpaRepository<Vessel, VesselComposite>{
+	List<Vessel> findByBthgDtContaining(String date);
+    String deleteByBthgDtContaining(String date);
+    Vessel findByAbbrVslMAndInVoyN(String abbrVslM, String inVoyN);
+    void deleteByAbbrVslMAndInVoyN(String abbrVslM, String inVoyN);
+    void deleteByAbbrVslMAndOutVoyN(String abbrVslM, String outVoyN);
 }
--- cs102-g1t2/src/main/java/g1t2/service/VesselService.java
+++ cs102-g1t2/src/main/java/g1t2/service/VesselService.java
@@ -11,75 +11,40 @@ import g1t2.repositories.VesselRepository;
 @Service
 public class VesselService {
 
-<<<<<<< Updated upstream
-	private VesselRepository vesselRepository;
-	
-	public List<Vessel> getAllVessels() {
-		List<Vessel> vessels = new ArrayList<>();
-		vesselRepository.findAll().forEach(vessels::add);
-		return vessels;
-	}
-	
-	public Vessel getVesselByKey(String abbrVslM, String inVoyN, String outVoyN) {
-		if (outVoyN == null) {
-			return vesselRepository.findByAbbrVoy(abbrVslM, inVoyN);
-		}
-		return vesselRepository.findByAbbrVoy(abbrVslM, outVoyN);
-	}
-	
-	public String addVessel(Vessel vessel) {
-		if (getVesselByKey(vessel.getAbbrVslM(), vessel.getInVoyN(), vessel.getOutVoyN()) != null) {
-			return "Adding vessel unsuccessful. Vessel already exists.";
-		}
-		vesselRepository.save(vessel);
-		return "Vessel added successfully.";
-	}
-	
-	public String updateVessel(String abbrVslM, String inVoyN, String outVoyN, Vessel vessel) {
-		vesselRepository.save(vessel);
-		return "Vessel successfully updated.";
-	}
-	
-	public String deleteVessel(String abbrVslM, String inVoyN, String outVoyN) {
-		if (getVesselByKey(abbrVslM, inVoyN, outVoyN) != null) {
-			if (outVoyN == null) {
-				vesselRepository.deleteByAbbrVoy(abbrVslM, inVoyN);
-			} else {
-				vesselRepository.deleteByAbbrVoy(abbrVslM, outVoyN);
-			}
-			return "Vessel successfully deleted.";
-		}
-		return "Vessel deletion unsuccessful. Vessel doesn't exist.";
-	}
-=======
-//	private VesselRepository vesselRepository;
-//	
-//	public List<Vessel> getAllVessels() {
-//		List<Vessel> vessels = new ArrayList<>();
-//		vesselRepository.findAll().forEach(vessels::add);
-//		return vessels;
-//	}
-//	
-//	public Vessel getVesselByKey(String abbrVslM, String inVoyN, String outVoyN) {
-//		if (outVoyN == null) {
-//			return vesselRepository.findByAbbr_InVoy(abbrVslM, inVoyN);
-//		}
-//		return vesselRepository.findByAbbr_InVoy(abbrVslM, outVoyN);
-//	}
-//	
-//	public String addVesselByKey(Vessel vessel) {
-//		if (getVesselByKey(vessel.getAbbrVslM(), vessel.getInVoyN(), vessel.getOutVoyN()) != null) {
-//			return "Adding vessel unsuccessful. Vessel already exists.";
-//		}
-//		vesselRepository.save(vessel);
-//		return "Vessel added successfully.";
-//	}
-//	
-//	public String updateVessel(Vessel vessel) {
-//		vesselRepository.save(vessel);
-//		return "Vessel successfully updated.";
-//	}
-//	
-//	public 
->>>>>>> Stashed changes
+ private VesselRepository vesselRepository;
+ 
+ public List<Vessel> getAllVessels() {
+  List<Vessel> vessels = new ArrayList<>();
+  vesselRepository.findAll().forEach(vessels::add);
+  return vessels;
+ }
+ 
+ public Vessel findByAbbrVslMAndInVoyN(String abbrVslM, String inVoyN){
+     return vesselRepository.findByAbbrVslMAndInVoyN(abbrVslM, inVoyN);
+ }
+ 
+ public String addVessel(Vessel vessel) {
+  if (findByAbbrVslMAndInVoyN(vessel.getAbbrVslM(), vessel.getInVoyN()) != null) {
+   return "Adding vessel unsuccessful. Vessel already exists.";
+  }
+  vesselRepository.save(vessel);
+  return "Vessel added successfully.";
+ }
+ 
+ public String updateVessel(String abbrVslM, String inVoyN, String outVoyN, Vessel vessel) {
+  vesselRepository.save(vessel);
+  return "Vessel successfully updated.";
+ }
+ 
+ public String deleteVessel(String abbrVslM, String inVoyN, String outVoyN) {
+  if (findByAbbrVslMAndInVoyN(abbrVslM, inVoyN) != null) {
+   if (outVoyN == null) {
+    vesselRepository.deleteByAbbrVslMAndInVoyN(abbrVslM, inVoyN);
+   } else {
+    vesselRepository.deleteByAbbrVslMAndOutVoyN(abbrVslM, outVoyN);
+   }
+   return "Vessel successfully deleted.";
+  }
+  return "Vessel deletion unsuccessful. Vessel doesn't exist.";
+ }
 }
