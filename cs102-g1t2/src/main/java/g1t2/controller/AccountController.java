479c13aeb89ab3178ff0e5a1759849cc90b283f4
harshit <harsheetjain>
2021-03-27 03:41:31 +0800

    Edited Account

--- cs102-g1t2/src/main/java/g1t2/controller/AccountController.java
+++ cs102-g1t2/src/main/java/g1t2/controller/AccountController.java
@@ -3,6 +3,7 @@ package g1t2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,27 +21,27 @@ public class AccountController {
 	private AccountService accountService;
 	
 	@RequestMapping("/accounts")
-	public List<Account> getAllAccounts() {
+	public ResponseEntity<List<Account>> getAllAccounts() {
 		return accountService.getAllAccounts();
 	}
 	
 	@RequestMapping("/accounts/{email}")
-	public Account getAccount(@PathVariable String email) {
+	public ResponseEntity<Account> getAccount(@PathVariable String email) {
 		return accountService.getAccountByEmail(email);
 	}
 	
 	@RequestMapping(method=RequestMethod.POST, value="/accounts")
-	public String addAccount(@RequestBody Account account) {
+	public ResponseEntity<Account> addAccount(@RequestBody Account account) {
 		return accountService.addAccount(account);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/accounts/{email}")
-	public String updateAccount(@RequestBody Account account, @PathVariable String email) {
-		return accountService.updateAccount(email, account);
+	@RequestMapping(method=RequestMethod.PUT, value="/accounts")
+	public ResponseEntity<Account> updateAccount(@RequestBody Account account) {
+		return accountService.updateAccount(account);
 	}
 	
 	@RequestMapping(method=RequestMethod.DELETE, value="/accounts/{email}")
-	public String deleteAccount(@PathVariable String email) {
+	public ResponseEntity<Void> deleteAccount(@PathVariable String email) {
 		return accountService.deleteAccount(email);
 	}
 }
--- cs102-g1t2/src/main/java/g1t2/service/AccountService.java
+++ cs102-g1t2/src/main/java/g1t2/service/AccountService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import g1t2.entities.Account;
@@ -15,36 +17,46 @@ public class AccountService {
 	@Autowired
 	private AccountRepository accountRepository;
 	
-	public List<Account> getAllAccounts() {
+	public ResponseEntity<List<Account>> getAllAccounts() {
 		List<Account> accounts = new ArrayList<>();
 		accountRepository.findAll()
 		.forEach(accounts::add);
-		return accounts;
+		return ResponseEntity.ok(accounts);
 	}
 	
-	public Account getAccountByEmail(String email) {
-		return accountRepository.findByEmail(email);
+	public ResponseEntity<Account> getAccountByEmail(String email) {
+		Account account = accountRepository.findByEmail(email);
+		if (account == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.ok(account);
 	}
 	
-	public String addAccount(Account account) {
-		if (getAccountByEmail(account.getEmail()) != null) {
-			return "Account creation unsuccessful. Account already exists.";
+	public ResponseEntity<Account> addAccount(Account account) {
+		HttpStatus accountExists = getAccountByEmail(account.getEmail()).getStatusCode();
+		if (accountExists == HttpStatus.OK) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 		accountRepository.save(account);
-		return "Account successfully created.";
+		return ResponseEntity.status(HttpStatus.CREATED).body(account);
 	}
 	
-	public String updateAccount(String email, Account account) {
-		accountRepository.save(account);
-		return "Account update successful.";
-	}
-	
-	public String deleteAccount(String email) {
-		if (getAccountByEmail(email) != null) {
-			accountRepository.deleteById(email);
-			return "Account delete successful.";
+	public ResponseEntity<Account> updateAccount(Account account) {
+		HttpStatus accountExists = getAccountByEmail(account.getEmail()).getStatusCode();
+		if (accountExists == HttpStatus.NOT_FOUND) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
-		return "Account delete unsuccessful. Account doesn't exist.";
+		accountRepository.save(account);
+		return ResponseEntity.ok(account);
+	}
+	
+	public ResponseEntity<Void> deleteAccount(String email) {
+		HttpStatus accountExists = getAccountByEmail(email).getStatusCode();
+		if (accountExists == HttpStatus.NOT_FOUND) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		accountRepository.deleteById(email);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
 }
