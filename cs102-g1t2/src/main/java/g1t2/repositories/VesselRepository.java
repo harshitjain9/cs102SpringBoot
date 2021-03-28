d4f14b09de01b0e227066e45a887794b2b6c92f7
charlotte-wt <charlottewee01@gmail.com>
2021-03-28 16:59:45 +0800

    Updated Schedule and Vessel Classes

--- cs102-g1t2/src/main/java/g1t2/repositories/VesselRepository.java
+++ cs102-g1t2/src/main/java/g1t2/repositories/VesselRepository.java
@@ -3,6 +3,7 @@ package g1t2.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import g1t2.entities.Vessel;
@@ -15,4 +16,13 @@ public interface VesselRepository extends JpaRepository<Vessel, VesselComposite>
     Vessel findByAbbrVslMAndInVoyN(String abbrVslM, String inVoyN);
     void deleteByAbbrVslMAndInVoyN(String abbrVslM, String inVoyN);
     void deleteByAbbrVslMAndOutVoyN(String abbrVslM, String outVoyN);
+    
+    // for SenseChangeInTime.java
+    String getFirstBthgDt(ResponseEntity<Vessel> vessel);
+    String getBthgDt(ResponseEntity<Vessel> vessel);
+    int getCount(ResponseEntity<Vessel> vessel);
+    void incrementCount(ResponseEntity<Vessel> vessel);
+	String getDisplayColor(ResponseEntity<Vessel> vessel);
+	String getUnbthgDt(ResponseEntity<Vessel> vessel);
+
 }
